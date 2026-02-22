cb51ac23176dc1b485dceedc63aca3ac89952d88
thandal <than@timbrel.org>
2026-02-22 07:20:27 -0500

    Not running on GCP (in general)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ echo 'PIP freeze (subset):'
 pip freeze | grep nvidia
 pip freeze | grep jax
 
-echo GCP instance:
-echo "Name:     $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/name || echo NA)"
-echo "Hostname: $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/hostname || echo NA)"
-echo "ID:       $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/id || echo NA)"
-echo "Zone:     $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/zone || echo NA)"
-echo
+#echo GCP instance:
+#echo "Name:     $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/name || echo NA)"
+#echo "Hostname: $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/hostname || echo NA)"
+#echo "ID:       $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/id || echo NA)"
+#echo "Zone:     $(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/zone || echo NA)"
+#echo
 
 echo GPUs:
 nvidia-smi --query-gpu=gpu_name,memory.total,driver_version --format=csv || true
